4cf9b6d0a62f89970d8268684ae6ed80d5671700
nishanth1920 <nicknishanth100@gamil.com>
2024-03-15 14:11:00 +0530

    main

--- read.php
+++ read.php
@@ -5,22 +5,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Read Page</title>
+    <!-- cdn for pagination and sort implementation in way better -->
+    <link href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- cdn for a bootstrap icon -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" integrity="sha384-4LISF5TTJX/fLmGSxO53rV4miRxdg84mZsxmO8Rx5jGtp/LbrixFETvWa5a6sESd" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <style>
+        div {
+            margin-bottom: 5px;
+        }
+    </style>
 </head>
 
 <body style="background-image: url(https://img.freepik.com/free-vector/gradient-blur-pink-blue-abstract-background_53876-117324.jpg?w=740&t=st=1710478849~exp=1710479449~hmac=c1b2c5635922a7aaf7cef296aa08fd60ee84c8f446efb780c0951b07b2b7289a);">
 
     <body>
 
-        <div class="container" style="margin-top: 20px;">
-            <div style="margin-bottom: 20px;">
+        <div class="container" style="margin-top: 20px; ">
+            <div>
                 <h3>Uploaded Data</h3>
             </div>
-            <div style="margin-bottom: 20px;">
-                <a href="index.php" class="btn btn-primary">Add User</a>
+            <div class="d-flex justify-content-end">
+                <a href="index.php" class="btn btn-dark" data-bs-toggle="tooltip" data-bs-placement="left" title="" data-bs-original-title="Go to the Page" style="margin-bottom: 10px;">Add User</a>
             </div>
-            <table class="table table-hover" id="userTable">
+            <table class="table table-hover" id="userTable" style="cursor: pointer;">
                 <thead>
                     <tr>
                         <th>ID</th>
@@ -61,8 +71,8 @@
                             echo "<td>" . $row["country"] . "</td>";
                             echo "<td>" . $row["password"] . "</td>";
                             echo "<td>
-                                    <a href='edit.php?id=" . $row["id"] . "' class='btn btn-primary btn-sm'>Edit</a>
-                                    <a href='delete.php?id=" . $row["id"] . "' class='btn btn-danger btn-sm'>Delete</a>
+                                    <a href='edit.php?id=" . $row["id"] . "' class='btn btn-primary btn-sm'><i class='bi bi-pencil'></i> Edit</a>
+                                    <a href='delete.php?id=" . $row["id"] . "' class='btn btn-danger btn-sm'><i class='bi bi-trash'></i> Delete</a>
                                   </td>";
                             echo "</tr>";
                         }
@@ -78,13 +88,23 @@
         <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
         <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/1.11.5/js/dataTables.bootstrap5.min.js"></script>
-      <script>
-        $(document).ready(function() {
-            $('#userTable').DataTable({
-                "lengthMenu": [[5, 25, 50, -1], [5, 25, 50, "All"]] // Custom entries for number of records per page
+        <script>
+            $(document).ready(function() {
+                $('#userTable').DataTable({
+                    "lengthMenu": [
+                        [-1, 20, 15, 10],
+                        ["All",20,15,10]
+                    ] // Custom entries for number of records per page
+                });
             });
-        });
-    </script>
+        </script>
+        <!-- script to activate Tooltip -->
+        <script>
+            var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+            var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+                return new bootstrap.Tooltip(tooltipTriggerEl)
+            })
+        </script>
 
 
     </body>
